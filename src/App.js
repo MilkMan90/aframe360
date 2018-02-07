54a1d60192985eb65e662cc9b924a022720a99b7
Matt Kaufman <matthew.leo.kaufman@gmail.com>
2018-02-07 15:19:51 -0700

    adds a buch of graphics capabilities and reactive data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,38 @@ import React, { Component } from 'react';
 
 import PanoFrame from './components/PanoFrame'
 
+function generateRandom(min, max) {
+  return min + Math.random()*(max+1 - min)
+}
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      data: Math.random(),
+      components: [],
+      renders: []
     }
+  }
+  generateNewObject() {
+    const newObj = {
+      x: generateRandom(-20, 20),
+      y: generateRandom(-20, 20),
+      z: generateRandom(-2, 10)
+    }
+    this.setState({
+      components: this.state.components.concat(newObj)
+    })
+  }
+  generateNewRender() {
+    const newObj = {
+      x: generateRandom(-20, 20),
+      y: generateRandom(-20, 20),
+      z: generateRandom(-2, 5)
+    }
+    this.setState({
+      renders: this.state.renders.concat(newObj)
+    })
   }
   render() {
     return (
@@ -20,7 +47,48 @@ class App extends Component {
           height: '100%',
           width: '100%',
         }}>
-        <PanoFrame />
+        <PanoFrame data={this.state.data} components={this.state.components}
+        renders={this.state.renders}/>
+        <button
+          onClick={()=>{this.setState({
+          data: Math.random()})}}
+          style={{
+            position: 'absolute',
+            left: '20px',
+            top: '10px',
+            width: '200px',
+            height: '50px',
+            zIndex: 9999
+          }}
+        >
+          Click Me
+        </button>
+        <button
+          onClick={()=>{this.generateNewObject()}}
+          style={{
+            position: 'absolute',
+            right: '300px',
+            top: '10px',
+            width: '200px',
+            height: '50px',
+            zIndex: 9999
+          }}
+        >
+          Add Text
+        </button>
+        <button
+          onClick={()=>{this.generateNewRender()}}
+          style={{
+            position: 'absolute',
+            right: '20px',
+            top: '10px',
+            width: '200px',
+            height: '50px',
+            zIndex: 9999
+          }}
+        >
+          Add Renders
+        </button>
       </div>
     );
   }
--- src/components/PanoFrame.js
+++ src/components/PanoFrame.js
@@ -3,6 +3,7 @@ import AFrame from 'aframe'
 import 'aframe-orbit-controls-component-2'
 // import createComponent from '../aframeComponents/orbitControls';
 import 'aframe-cubemap-component'
+import 'aframe-look-at-component'
 import '../App.css'
 import React, { Component } from 'react';
 
@@ -11,6 +12,8 @@ import ArrowMtl from '../objects/arrow/arrow.mtl'
 
 import MSObj from '../objects/mapspacer/302.obj';
 import MSMtl from '../objects/mapspacer/302.mtl';
+import MSdae from '../objects/mapspacer/302.dae';
+
 
 // import pano1 from '../panos/Erdman Hall common room.jpg'
 import pano2 from '../panos/FEAST Organic Garden.jpg'
@@ -42,6 +45,7 @@ class PanoFrame extends Component {
     }
   }
   componentDidMount() {
+
     // const camera = document.querySelector('#camera')
     // // camera.setAttribute('orbit-controls', 'getZoomScale', function(test) {
     // //   console.log(test)
@@ -57,7 +61,6 @@ class PanoFrame extends Component {
     // // const test = camera.getAttribute('orbit-controls').getZoom
     // // console.log(test);
 
-    console.log(document.querySelector('#sky').components);
 
     const assetEl = document.querySelector('#pano1')
     assetEl.addEventListener('load', function () {
@@ -93,7 +96,28 @@ class PanoFrame extends Component {
   }
   initializeZoom() {
     document.querySelector('#scene').addEventListener('mousewheel', (event) => {
-      this.changeZoom(event.deltaY);
+      console.log(event);
+      this.changeZoom(event.deltaY, 0.05);
+      // let newZoom = 1;
+      // const zoomVelocity = 0.1;
+      // if (event.deltaY < 0) {
+      //   newZoom = this.state.zoom + zoomVelocity;
+      // }
+      // // if (event.deltaY > 0 && this.state.zoom >= 1) {
+      // if (event.deltaY > 0) {
+      //   newZoom = this.state.zoom - zoomVelocity;
+      // }
+      //
+      // // if (newZoom < 1) {
+      // //   newZoom = 1;
+      // // }
+      //
+      // this.setState({
+      //   zoom: newZoom
+      // });
+    })
+    document.querySelector('#scene').addEventListener('wheel', (event) => {
+      this.changeZoom(event.deltaY, 0.5);
       // let newZoom = 1;
       // const zoomVelocity = 0.1;
       // if (event.deltaY < 0) {
@@ -113,9 +137,10 @@ class PanoFrame extends Component {
       // });
     })
   }
-  changeZoom(deltaY) {
+  changeZoom(deltaY, browserMultiplier) {
+
     const camera = document.querySelector('#camera').components.camera.camera;
-		var fov = camera.fov + deltaY * 0.05;
+		var fov = camera.fov + deltaY * browserMultiplier;
 		camera.fov = THREE.Math.clamp( fov, 10, 75 );
 		camera.updateProjectionMatrix();
   }
@@ -161,9 +186,28 @@ class PanoFrame extends Component {
 
   }
 
+  renderTextObjects(components) {
+    return components.map(component => {
+      return <a-text look-at="[camera]"
+        value={this.props.data * Math.random()}
+        text={`width:20;color:${'#'+Math.floor(Math.random()*16777215).toString(16)};side:double`}
+        position={`${component.x} ${component.y} ${component.z}`}
+        rotation="-4.011 -126.108 0"></a-text>
+    })
+  }
+  renderObjects(objects) {
+    return objects.map(object => {
+        return <a-gltf-model src="url(https://s3.us-east-2.amazonaws.com/mlktestbucket/street/scene.gltf)"
+        gltf-model="https://s3.us-east-2.amazonaws.com/mlktestbucket/street/scene.gltf"
+        position={`${object.x} ${object.y} ${object.z}`}
+        rotation="0 -94.25155729902042 0" scale="0.01 0.01 0.01"></a-gltf-model>
+    })
+  }
   render() {
     const selected = this.state.selectedArrow;
 
+    const textObjects = this.props.components.length > 0 && this.renderTextObjects(this.props.components);
+    const renderObjects = this.props.renders.length > 0 && this.renderObjects(this.props.renders);
     return (
       <div
         className="App"
@@ -172,12 +216,15 @@ class PanoFrame extends Component {
           height: '100%',
           width: '100%',
         }}>
-        <a-scene id="scene" cursor="rayOrigin: mouse">
+        <a-scene id="scene" stats cursor="rayOrigin: mouse">
           <a-assets>
             <a-asset-item id="arrow-obj" src={ArrowObj}></a-asset-item>
             <a-asset-item id="arrow-mtl" src={ArrowMtl}></a-asset-item>
-            <a-asset-item id="ms-obj" src={MSObj}></a-asset-item>
-            <a-asset-item id="ms-mtl" src={MSMtl}></a-asset-item>
+            <a-asset-item id="street1" src="https://s3.us-east-2.amazonaws.com/mlktestbucket/street/scene.gltf" crossorigin="anonymous"></a-asset-item>
+            {/* <a-asset-item id="street" src="https://dl.dropboxusercontent.com/s/dvahjq5trg9pysj/evil-skull.gltf"></a-asset-item> */}
+            {/* <a-asset-item id="ms-obj" src={MSObj}></a-asset-item>
+            <a-asset-item id="ms-mtl" src={MSMtl}></a-asset-item> */}
+            {/* <a-asset-item id="ms-dae" src={MSdae}></a-asset-item> */}
             <img id="pano1" crossorigin="anonymous" src={pano1}/>
             <img id="pano2" crossorigin="anonymous" src={pano2}/>
             <img id="pano3" crossorigin="anonymous" src={pano3}/>
@@ -194,7 +241,8 @@ class PanoFrame extends Component {
 
           <a-entity
             id="camera"
-            camera position="0 0 2"
+            camera
+            position="0 3 2"
             wasd-controls-enabled="false"
             look-controls
             orbit-controls="
@@ -210,13 +258,25 @@ class PanoFrame extends Component {
                       maxDistance: 2;
                       maxPolarAngle: 3.14;
                       autoVRLookCam: false"
-              user-height="1.6">
+              user-height="0">
           </a-entity>
-          {/* <a-camera
 
-          /> */}
+          {textObjects}
+          {renderObjects}
+
+          <a-text look-at="[camera]"
+            value={this.state.currentPano} text="color:#f2d209;opacity:0.8;width:20;side:double" position="2.674 1.363 -6.948" rotation="0.6302535746439055 -24.350706293059986 0"></a-text>
+
+          <a-text look-at="[camera]" value={this.props.data * Math.random()} text="width:20;color:#f996ee;side:double" position="20.619 -2.895 8.072" rotation="-4.011 -126.108 0"></a-text>
+          <a-text look-at="[camera]" value={this.props.data * Math.random()}  text="color:#00c6ee;width:20;side:double" position="18.24 -2.895 -4.431" rotation="-12.146705256773451 -75.22935850067708 0"></a-text>
+          <a-text look-at="[camera]" value={this.props.data * Math.random()}  text="color:#09efa6;width:20;side:double" position="9.157 -2.895 10.378" rotation="-12.719663051904275 -179.04931097838227 0"></a-text>
+              {/* <a-collada-model src="#ms-dae" collada-model="/static/media/302.042c8af9.dae" position="-6.036 -6.435 -10.286" rotation="22.345354010102106 -15.183381570966816 -5.901465289847479" scale="0.774 0.646 0.568"></a-collada-model>           */}
+            {/* <a-gltf-model src="#street"></a-gltf-model> */}
+            <a-gltf-model src="url(https://s3.us-east-2.amazonaws.com/mlktestbucket/street/scene.gltf)" gltf-model="https://s3.us-east-2.amazonaws.com/mlktestbucket/street/scene.gltf" position="32.377 -5.084 -6.129" rotation="0 -94.25155729902042 0" scale="0.01 0.01 0.01"></a-gltf-model>
+            {/* <a-entity gltf-model="#street1"></a-entity> */}
+
           <a-entity>
-            <a-entity id="target" scale="1 1 1" position="0 3 0"></a-entity>
+            <a-entity id="target" scale="1 1 1" position="0 1.5 0"></a-entity>
             {/* <a-entity obj-model="obj: #ms-obj; mtl: #ms-mtl;"  scale="0.5 0.5 0.5" position="54.841 -5.266 3" rotation="-55.748793466229095 -89.09493714284301 -31.569974511708363"></a-entity> */}
             <a-entity class="arrow" id="arrow1" obj-model="obj: #arrow-obj;"
               material={`color: ${selected === 'arrow1' ? 'springgreen' : 'lightgray'}`}
